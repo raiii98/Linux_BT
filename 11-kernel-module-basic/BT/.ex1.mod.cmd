01b6c2ec059f2ad28e13dba3a6d51c27e5e3c260
raiii <quoctoan24898@gmail.com>
2023-06-25 00:14:56 +0700

    c15 error read

--- 11-kernel-module-basic/BT/.ex1.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/raiii/Linux_BT/11-kernel-module-basic/BT/ex1.mod := { echo  /home/raiii/Linux_BT/11-kernel-module-basic/BT/ex1.o;  echo; } > /home/raiii/Linux_BT/11-kernel-module-basic/BT/ex1.mod
