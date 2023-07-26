16ef02436c40b00ed1af04e6707f74e0cd1a527e
raiii <quoctoan24898@gmail.com>
2023-07-27 00:08:01 +0700

    c31

--- 10-message-queues/BT/POSIX/client.c
+++ 10-message-queues/BT/POSIX/client.c
@@ -17,6 +17,20 @@
 
 int main(int argc, char *argv[])
 {
-
+    char ptr[]= "Hello my client 1\n";
     struct mq_attr client_attr;
+    client_attr.mq_flags = 0;
+    client_attr.mq_maxmsg = 10;
+    client_attr.mq_msgsize = BUFFER;
+    client_attr.mq_curmsgs = 0;
+    unsigned int client1_prio = 1;
+    mqd_t client_des = mq_open("/server1", O_RDWR | O_CREAT, MQ_MODE, &client_attr);
+
+    int client_send= mq_send(client_des,ptr,strlen(ptr),client1_prio);
+    if(client_send==-1){
+        handle_error("mq_send");
+        exit(EXIT_FAILURE);
+
+    }
+    return 0;
 }
--- 10-message-queues/BT/POSIX/server.c
+++ 10-message-queues/BT/POSIX/server.c
@@ -4,7 +4,7 @@
 #include <fcntl.h>  // Thư viện cung cấp các cờ (O_RDONLY,...)
 #include <string.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 #define BUFFER 256
 #define handle_error(msg)   \
     do                      \
@@ -19,25 +19,42 @@ int main(int argc, char *argv[])
 {
 
     struct mq_attr server_attr;
+    server_attr.mq_flags = 0;
+    server_attr.mq_maxmsg = 10;
+    server_attr.mq_msgsize = BUFFER;
+    server_attr.mq_curmsgs = 0;
+
     char client_str[BUFFER];
     unsigned int client_prio;
-    mqd_t server_des = mq_open("/server", O_RDWR | O_CREAT, MQ_MODE, server_attr);
+
+    mqd_t server_des  = mq_open("/server1", O_RDWR | O_CREAT, MQ_MODE, &server_attr);
     if (server_des == -1)
     {
         handle_error("mq_open");
         exit(EXIT_FAILURE);
     }
-
-    if (mq_receive(server_des, client_str, sizeof(client_str), &client_prio) == -1)
+    while (1)
+    {
+    ssize_t server_receive = mq_receive(server_des, client_str, sizeof(client_str), &client_prio);
+    if (server_receive == -1)
     {
         handle_error("mq_receive");
         exit(EXIT_FAILURE);
     }
-    else
+    else if(server_receive > 0)
     {
 
-        printf("%s", client_str);
+        printf("%s\n", client_str);
         sleep(1);
+        printf("%ld\n", server_receive);
+
     }
+
+    else if(server_receive == 0){
+        break;
+    }
+    }
+    
+    mq_unlink ("/server1");
     return 0;
 }
